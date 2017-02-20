58621ecd07e93a03b0808228eca764d0753de07b
Paul J. Lucas <plucas@splunk.com>
2017-02-20 14:49:37 -0800

    Intermediate commit.

--- src/ast.c
+++ src/ast.c
@@ -7,12 +7,19 @@
 #include "config.h"                     /* must go first */
 #include "ast.h"
 #include "literals.h"
+#include "options.h"
 #include "util.h"
 
 // system
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>                     /* for memset(3) */
+#include <sysexits.h>
+
+///////////////////////////////////////////////////////////////////////////////
+
+// local variable definitions
+static unsigned c_ast_count;            // allocated but not freed
 
 ////////// extern functions ///////////////////////////////////////////////////
 
@@ -44,23 +51,28 @@ void c_ast_check( c_ast_t *ast ) {
     case K_BUILTIN:
       // nothing to do
       break;
+    case K_ENUM_CLASS_STRUCT_UNION:
+      // TODO
+      break;
     case K_FUNCTION:
-      for ( c_ast_t *arg = ast->as.func.args.head; arg; arg = arg->next )
+      for ( c_ast_t *arg = ast->as.func.args.head_ast; arg; arg = arg->next )
         /* TODO */;
       break;
-    case K_MEMBER:
+    case K_PTR_TO_MEMBER:
       break;
     case K_NAME:
       break;
     case K_POINTER:
     case K_REFERENCE:
       break;
-    case K_STRUCT:
-      // TODO
-      break;
   } // switch
 }
 #endif
+
+void c_ast_cleanup( void ) {
+  if ( c_ast_count > 0 )
+    INTERNAL_ERR( "number of c_ast objects (%u) > 0\n", c_ast_count );
+}
 
 void c_ast_english( c_ast_t const *ast, FILE *fout ) {
   assert( ast );
@@ -74,7 +86,7 @@ void c_ast_english( c_ast_t const *ast, FILE *fout ) {
       if ( ast->as.array.size != C_ARRAY_NO_SIZE )
         FPRINTF( fout, "%d ", ast->as.array.size );
       FPRINTF( fout, "%s ", L_OF );
-      c_ast_english( ast->as.member.of_ast, fout );
+      c_ast_english( ast->as.array.of_ast, fout );
       break;
 
     case K_BLOCK:
@@ -84,27 +96,33 @@ void c_ast_english( c_ast_t const *ast, FILE *fout ) {
       FPUTS( c_type_name( ast->as.type ), fout );
       break;
 
+    case K_ENUM_CLASS_STRUCT_UNION:
+      FPUTS( c_type_name( ast->as.type ), fout );
+      if ( ast->name )
+        FPRINTF( fout, " %s", ast->name );
+      break;
+
     case K_FUNCTION:
       FPRINTF( fout, "%s (", L_FUNCTION );
-      for ( c_ast_t *arg = ast->as.func.args.head; arg; arg = arg->next )
+      for ( c_ast_t *arg = ast->as.func.args.head_ast; arg; arg = arg->next )
         c_ast_english( arg, fout );
       FPRINTF( fout, ") %s ", L_RETURNING );
       c_ast_english( ast->as.func.ret_ast, fout );
       break;
 
-    case K_MEMBER:
-      if ( ast->as.member.qualifier )
-        FPRINTF( fout, "%s ", c_type_name( ast->as.member.qualifier ) );
+    case K_PTR_TO_MEMBER:
+      if ( ast->as.ptr_mbr.qualifier )
+        FPRINTF( fout, "%s ", c_type_name( ast->as.ptr_mbr.qualifier ) );
       FPRINTF( fout,
         "%s %s %s %s %s %s ",
-        L_POINTER, L_TO, L_MEMBER, L_OF, L_CLASS, ast->as.member.class_name
+        L_POINTER, L_TO, L_MEMBER, L_OF, L_CLASS, ast->as.ptr_mbr.class_name
       );
-      c_ast_english( ast->as.member.of_ast, fout );
+      c_ast_english( ast->as.ptr_mbr.of_ast, fout );
       break;
 
     case K_NAME:
-      if ( ast->as.name )
-        FPUTS( ast->as.name, fout );
+      if ( ast->name )
+        FPUTS( ast->name, fout );
       break;
 
     case K_POINTER:
@@ -116,15 +134,18 @@ void c_ast_english( c_ast_t const *ast, FILE *fout ) {
       );
       c_ast_english( ast->as.ptr_ref.to_ast, fout );
       break;
-
-    case K_STRUCT:
-      // TODO
-      break;
   } // switch
 }
 
 void c_ast_free( c_ast_t *ast ) {
-  assert( ast );
+  if ( ast == NULL )
+    return;
+  //assert( c_ast_count > 0 );
+
+  --c_ast_count;
+
+  if ( ast->name )
+    FREE( ast->name );
 
   switch ( ast->kind ) {
     case K_NONE:
@@ -133,33 +154,51 @@ void c_ast_free( c_ast_t *ast ) {
       if ( ast->as.array.of_ast )
         c_ast_free( ast->as.array.of_ast );
       break;
-    case K_BLOCK:
-      // TODO
-      break;
     case K_BUILTIN:
       // nothing to do
       break;
+    case K_ENUM_CLASS_STRUCT_UNION:
+      // nothing to do
+      break;
+    case K_BLOCK:
     case K_FUNCTION:
-      for ( c_ast_t *arg = ast->as.func.args.head; arg; ) {
+      for ( c_ast_t *arg = ast->as.func.args.head_ast; arg; ) {
         c_ast_t *const next = arg->next;
         c_ast_free( arg );
         arg = next;
       }
       break;
-    case K_MEMBER:
-      free( (void*)ast->as.member.class_name );
-      c_ast_free( ast->as.member.of_ast );
+    case K_PTR_TO_MEMBER:
+      FREE( ast->as.ptr_mbr.class_name );
+      c_ast_free( ast->as.ptr_mbr.of_ast );
       break;
     case K_NAME:
-      free( (void*)ast->as.name );
+      // nothing to do
       break;
     case K_POINTER:
     case K_REFERENCE:
       c_ast_free( ast->as.ptr_ref.to_ast );
       break;
-    case K_STRUCT:
-      // TODO
-      break;
+  } // switch
+}
+
+char const* c_ast_name( c_ast_t const *ast ) {
+  if ( !ast )
+    return NULL;
+
+  if ( ast->name )
+    return ast->name;
+
+  switch ( ast->kind ) {
+    case K_ARRAY:
+      return c_ast_name( ast->as.array.of_ast );
+    case K_PTR_TO_MEMBER:
+      return NULL;
+    case K_POINTER:
+    case K_REFERENCE:
+      return c_ast_name( ast->as.ptr_ref.to_ast );
+    default:
+      return NULL;
   } // switch
 }
 
@@ -167,7 +206,33 @@ c_ast_t* c_ast_new( c_kind_t kind ) {
   c_ast_t *const ast = MALLOC( c_ast_t, 1 );
   memset( ast, 0, sizeof( c_ast_t ) );
   ast->kind = kind;
+  ++c_ast_count;
   return ast;
+}
+
+char const* c_kind_name( c_kind_t kind ) {
+  switch ( kind ) {
+    case K_NONE         : return "none";
+    case K_ARRAY        : return "array";
+    case K_BLOCK        : return "block";
+    case K_BUILTIN      : return "built-in type";
+    case K_FUNCTION     : return "function";
+    case K_NAME         : return "name";
+    case K_POINTER      : return "pointer";
+    case K_PTR_TO_MEMBER: return "pointer-to-member";
+    case K_REFERENCE    : return "reference";
+
+    case K_ENUM_CLASS_STRUCT_UNION:
+      if ( opt_lang >= LANG_CPP )
+        return "enum, class, struct, or union";
+      if ( opt_lang >= LANG_C_89 )
+        return "enum, struct, or union";
+      return "struct or union";
+
+    default:
+      INTERNAL_ERR( "unexpected value (%d) for c_kind\n", (int)kind );
+      return NULL;                      // suppress warning
+  } // switch
 }
 
 ///////////////////////////////////////////////////////////////////////////////
--- src/ast.h
+++ src/ast.h
@@ -20,12 +20,13 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 
-typedef struct c_array    c_array_t;
 typedef struct c_ast      c_ast_t;
 typedef struct c_ast_list c_ast_list_t;
+typedef struct c_array    c_array_t;
+typedef struct c_block    c_block_t;
 typedef struct c_func     c_func_t;
 typedef enum   c_kind     c_kind_t;
-typedef struct c_member   c_member_t;
+typedef struct c_ptr_mbr  c_ptr_mbr_t;
 typedef struct c_ptr_ref  c_ptr_ref_t;
 
 #define C_ARRAY_NO_SIZE   (-1)
@@ -38,36 +39,56 @@ enum c_kind {
   K_ARRAY,
   K_BLOCK,                              // Apple extension
   K_BUILTIN,                            // void, char, int, etc.
+  K_ENUM_CLASS_STRUCT_UNION,
   K_FUNCTION,
-  K_MEMBER,                             // C++ class data member
   K_NAME,
   K_POINTER,
+  K_PTR_TO_MEMBER,                      // C++ class data member
   K_REFERENCE,
-  K_STRUCT,                             // or union or C++ class
 };
 
 /**
  * Linked-list of AST objects.
  */
 struct c_ast_list {
-  c_ast_t  *head;
-  c_ast_t  *tail;
+  c_ast_t  *head_ast;
+  c_ast_t  *tail_ast;
 };
 
 /**
  * AST object for a C/C++ array.
  */
 struct c_array {
-  int       size;
   c_ast_t  *of_ast;
+  int       size;
+};
+
+/**
+ * AST object for a C/C++ block (Apple extension).
+ */
+struct c_block {
+  c_ast_t      *ret_ast;
+  c_ast_list_t  args;
+  c_type_t      type;
 };
 
 /**
  * AST object for a C/C++ function.
+ * (Note that the members are laid out in the same order as c_block: this is
+ * taken advantage of.)
  */
 struct c_func {
   c_ast_t      *ret_ast;
   c_ast_list_t  args;
+};
+
+/**
+ * AST object for a C++ pointer-to-member of class.
+ */
+struct c_ptr_mbr {
+  c_type_t    qualifier;                // T_CONST, T_RESTRICT, T_VOLATILE
+  c_ast_t    *of_ast;
+  char const *class_name;
 };
 
 /**
@@ -79,32 +100,31 @@ struct c_ptr_ref {
 };
 
 /**
- * AST object for a C++ pointer-to-class-member.
- */
-struct c_member {
-  c_type_t    qualifier;                // T_CONST, T_RESTRICT, T_VOLATILE
-  char const *class_name;
-  c_ast_t    *of_ast;
-};
-
-/**
  * AST object.
  */
 struct c_ast {
-  c_kind_t  kind;
-  c_ast_t  *next;
+  c_kind_t    kind;
+  char const *name;
+  c_ast_t    *next;
 
   union {
-    c_array_t     array;                // K_ARRAY
-    c_type_t      type;                 // K_BUILTIN
-    c_func_t      func;                 // K_FUNCTION
-    c_member_t    member;               // K_MEMBER
-    char const   *name;                 // K_NAME
-    c_ptr_ref_t   ptr_ref;              // K_POINTER or K_REFERENCE
+    c_array_t     array;
+    c_block_t     block;
+    c_type_t      type;
+    c_func_t      func;
+    c_ptr_mbr_t   ptr_mbr;
+    c_ptr_ref_t   ptr_ref;
   } as;
 };
 
 ////////// extern functions ///////////////////////////////////////////////////
+
+/**
+ * Cleans-up AST data.
+ * (Currently, this checks that the number of c_ast object freed match the
+ * number allocated.)
+ */
+void c_ast_cleanup( void );
 
 /**
  * Prints the given c_ast as English.
@@ -122,11 +142,27 @@ void c_ast_english( c_ast_t const *ast, FILE *fout );
 void c_ast_free( c_ast_t *ast );
 
 /**
+ * Gets the name from the given c_ast.
+ *
+ * @param ast The c_ast to get the name from.
+ * @return Returns said name or null if none.
+ */
+char const* c_ast_name( c_ast_t const *ast );
+
+/**
  * Creates a new c_ast.
  *
  * @param kind The kind of object to create.
  */
 c_ast_t* c_ast_new( c_kind_t kind );
+
+/**
+ * Gets the name of the given kind.
+ *
+ * @param kind The kind to get the name for.
+ * @return Returns said name.
+ */
+char const* c_kind_name( c_kind_t kind );
 
 ///////////////////////////////////////////////////////////////////////////////
 
