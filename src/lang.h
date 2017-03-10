9a83098054b47948ab500416625da252fc409670
Paul J. Lucas <plucas@splunk.com>
2017-03-09 21:38:27 -0800

    Added 'u'.

--- src/lang.h
+++ src/lang.h
@@ -14,19 +14,19 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 // languages supported
-#define LANG_NONE     0
-#define LANG_ALL      ~LANG_NONE
+#define LANG_NONE     (0u)
+#define LANG_ALL      (~LANG_NONE)
 
-#define LANG_C_KNR    (1 << 0)
-#define LANG_C_89     (1 << 1)
-#define LANG_C_95     (1 << 2)
-#define LANG_C_99     (1 << 3)
-#define LANG_C_11     (1 << 4)
+#define LANG_C_KNR    (1u << 0)
+#define LANG_C_89     (1u << 1)
+#define LANG_C_95     (1u << 2)
+#define LANG_C_99     (1u << 3)
+#define LANG_C_11     (1u << 4)
 #define LANG_C_MAX    LANG_C_11
 
 #define LANG_CPP_MIN  LANG_CPP_03
-#define LANG_CPP_03   (1 << 5)
-#define LANG_CPP_11   (1 << 6)
+#define LANG_CPP_03   (1u << 5)
+#define LANG_CPP_11   (1u << 6)
 #define LANG_CPP_MAX  LANG_CPP_11
 #define LANG_CPP_ANY  (LANG_CPP_03 | LANG_CPP_11)
 
