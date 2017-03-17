b04939d395d0ec123c2706d4f1877662156040f0
Paul J. Lucas <plucas@splunk.com>
2017-03-17 10:53:55 -0700

    Tweaked help.

--- src/help.c
+++ src/help.c
@@ -31,34 +31,36 @@ typedef struct help_text help_text_t;
  */
 static help_text_t const HELP_TEXT[] = {
 /*  1 */  { "| = alternate; [] = optional; {} = 1 or more; <> = defined elsewhere", NULL },
-/*  2 */  { "  commands are separated by ';' or newline", NULL },
-/*  3 */  { "command:", NULL },
-/*  4 */  { "  declare <name> as <english>", NULL },
-/*  5 */  { "  cast <name> into <english>", NULL },
-/*  6 */  { "  explain <gibberish>", NULL },
-/*  7 */  { "  set [options]", NULL },
-/*  8 */  { "  help | ?", NULL },
-/*  9 */  { "  exit | quit", NULL },
-/* 10 */  { "english:", NULL },
-/* 11 */  { "  array [<number>] of <english>", NULL },
-/* 12 */  { "  block [( <decl-list> )] returning <english>", NULL },
-/* 13 */  { "  function [( <decl-list> )] returning <english>", NULL },
-/* 14 */  { "  [{ const[ant] | volatile | restrict[ed] }] pointer to <english>",
-            "  [{const|volatile}] {pointer|ref[erence]} to [member of class <name>] <english>" },
-/* 15 */  { "  <type>", NULL },
-/* 16 */  { "type:", NULL },
-/* 17 */  { "  {[<storage-class>] [{<modifier>}] [<C-type>]}", NULL },
-/* 18 */  { "  { struct | union | enum } <name>",
-            "  {struct|class|union|enum} <name>" },
-/* 19 */  { "decl-list: a comma separated list of <name>, <english>, or <name> as <english>", NULL },
-/* 20 */  { "name: a C identifier",
+/*  2 */  { "command:", NULL },
+/*  3 */  { "  declare <name> as <english>", NULL },
+/*  4 */  { "  cast <name> into <english>", NULL },
+/*  5 */  { "  explain <gibberish>", NULL },
+/*  6 */  { "  set [options]", NULL },
+/*  7 */  { "  help | ?", NULL },
+/*  8 */  { "  exit | quit", NULL },
+/*  9 */  { "english:", NULL },
+/* 10 */  { "  array [<number>] of <english>", NULL },
+/* 11 */  { "  block [( [<decl-list>] )] returning <english>", NULL },
+/* 12 */  { "  function [( [<decl-list>] )] returning <english>", NULL },
+/* 13 */  { "  [{ const[ant] | volatile | restrict[ed] }] pointer to <english>",
+            "  [{const|volatile}] {pointer|reference} to [member of class <name>] <english>" },
+/* 14 */  { "  <type>", NULL },
+/* 15 */  { "type:", NULL },
+/* 16 */  { "  [<storage-class>] [{<modifier>}] [<C-type>]",
+            "  [<storage-class>] [{<modifier>}] [<C++-type>]" },
+/* 17 */  { "  { enum | struct | union } <name>",
+            "  { enum | struct | union | class } <name>" },
+/* 18 */  { "decl-list: a comma separated list of <name>, <english>, or <name> as <english>", NULL },
+/* 19 */  { "name: a C identifier",
             "name: a C++ identifier" },
-/* 21 */  { "gibberish: a C declaration, like \"int *x\", or cast, like \"(int*)x\"", NULL },
-/* 22 */  { "storage-class: auto, extern, register, static, or _Thread_local",
+/* 20 */  { "gibberish: a C declaration, like \"int *x\", or cast, like \"(int*)x\"",
+            "gibberish: a C++ declaration, like \"int *x\", or cast, like \"(int*)x\"" },
+/* 21 */  { "C-type: bool, int, char, char16_t, char32_t, wchar_t, float, double, or void",
+            "C++-type: bool, int, char, char16_t, char32_t, wchar_t, float, double, or void" },
+/* 22 */  { "modifier: short, long, signed, unsigned, const, volatile, or restrict",
+            "modifier: short, long, signed, unsigned, const, or volatile" },
+/* 23 */  { "storage-class: auto, extern, register, static, or _Thread_local",
             "storage-class: extern, register, static, or thread_local" },
-/* 23 */  { "C-type: bool, int, char, char16_t, char32_t, wchar_t, float, double, or void", NULL },
-/* 24 */  { "modifier: short, long, signed, unsigned, const, volatile, or restrict",
-          "modifier: short, long, signed, unsigned, const, or volatile" },
           { NULL, NULL }
 };
 
@@ -70,7 +72,7 @@ static help_text_t const HELP_TEXT[] = {
  * @param line The line to print.
  */
 static void print_help_line( char const *line ) {
-  FPUTS( "  ", stdout );
+  FPUTC( ' ', stdout );
   for ( char const *c = line; *c; ++c ) {
     switch ( *c ) {
       case '<':
