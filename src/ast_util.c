d09c0244051b3761f6fd76ec7f127d6d11962838
Paul J. Lucas <plucas@splunk.com>
2017-03-25 21:09:49 -0700

    Split off c_ast_check().

--- src/ast_util.c
+++ src/ast_util.c
@@ -7,13 +7,9 @@
 #include "config.h"                     /* must go first */
 #define CDECL_AST_UTIL_INLINE _GL_EXTERN_INLINE
 #include "ast_util.h"
-#include "literals.h"
 
 // standard
 #include <assert.h>
-
-// local functions
-static bool c_ast_check_impl( c_ast_t const* );
 
 ////////// local functions ////////////////////////////////////////////////////
 
@@ -129,83 +125,6 @@ static c_ast_t* c_ast_add_func_impl( c_ast_t *ast, c_ast_t *ret_type_ast,
 }
 
 /**
- * Performs additional checks on an entire AST for semantic validity when
- * casting.
- *
- * @param ast The AST to check.
- * @return Returns \c true only if the AST does not violate any cast checks.
- */
-static bool c_ast_check_cast( c_ast_t const *ast ) {
-  assert( ast );
-  c_ast_t *const nonconst_ast = CONST_CAST( c_ast_t*, ast );
-
-  c_ast_t const *const storage_ast =
-    c_ast_find_type( nonconst_ast, V_DOWN, T_STORAGE );
-
-  if ( storage_ast ) {
-    c_type_t const storage = storage_ast->type & T_MASK_STORAGE;
-    print_error( &ast->loc, "can not cast to %s", c_type_name( storage ) );
-    return false;
-  }
-
-  switch ( ast->kind ) {
-    case K_ARRAY:
-      print_error( &ast->loc, "cast into array" );
-      print_hint( "cast into pointer" );
-      return false;
-    case K_FUNCTION:
-      print_error( &ast->loc, "cast into function" );
-      print_hint( "cast into pointer to function" );
-      return false;
-    default:
-      return true;
-  } // switch
-}
-
-/**
- * Checks all function (or block) arguments for semantic validity.
- *
- * @param ast The function (or block) AST to check.
- * @return Returns \c true only if all function arguments are valid.
- */
-static bool c_ast_check_func_args( c_ast_t const *ast ) {
-  assert( ast );
-  assert( ast->kind & (K_BLOCK | K_FUNCTION) );
-
-  c_ast_t const *arg, *void_arg = NULL;
-  unsigned n_args;
-
-  for ( arg = c_ast_args( ast ), n_args = 1; arg; arg = arg->next, ++n_args ) {
-    if ( n_args > 1 && void_arg )
-      goto only_void;
-
-    if ( arg->kind == K_BUILTIN && (arg->type & T_VOID) ) {
-      //
-      // Ordinarily, void variables are invalid; but a single void function
-      // "argument" is valid (as long as it doesn't have a name).
-      //
-      if ( arg->name ) {
-        print_error( &arg->loc, "argument of void" );
-        return false;
-      }
-      void_arg = arg;
-      if ( n_args > 1 )
-        goto only_void;
-      continue;
-    }
-
-    if ( !c_ast_check_impl( arg ) )
-      return false;
-  } // for
-
-  return true;
-
-only_void:
-  print_error( &void_arg->loc, "\"void\" must be only parameter if specified" );
-  return false;
-}
-
-/**
  * Takes the storage type, if any, away from \a ast
  * (with the intent of giving it to another c_ast).
  * This is used is cases like:
@@ -281,114 +200,6 @@ c_ast_t* c_ast_add_func( c_ast_t *ast, c_ast_t *ret_type_ast, c_ast_t *func ) {
   assert( rv );
   func->type |= c_ast_take_storage( func->as.func.ret_ast );
   return rv;
-}
-
-static bool c_ast_check_impl( c_ast_t const *ast ) {
-  assert( ast );
-
-  switch ( ast->kind ) {
-    case K_ARRAY: {
-      c_ast_t const *const of_ast = ast->as.array.of_ast;
-      switch ( of_ast->kind ) {
-        case K_BUILTIN:
-          if ( of_ast->type & T_VOID ) {
-            print_error( &ast->loc, "array of void" );
-            print_hint( "array of pointer to void" );
-            return false;
-          }
-          if ( of_ast->type & T_REGISTER ) {
-            print_error( &ast->loc, "array can not be register" );
-            return false;
-          }
-          break;
-        case K_FUNCTION:
-          print_error( &ast->loc, "array of function" );
-          print_hint( "array of pointer to function" );
-          return false;
-        default:
-          return c_ast_check_impl( of_ast );
-      } // switch
-      break;
-    }
-
-    case K_BUILTIN:
-      if ( (ast->type & T_VOID) && !ast->parent ) {
-        print_error( &ast->loc, "variable of void" );
-        print_hint( "pointer to void" );
-        return false;
-      }
-      break;
-
-    case K_ENUM_CLASS_STRUCT_UNION:
-    case K_NAME:
-      // nothing to check
-      break;
-
-    case K_FUNCTION:
-      if ( ast->type & T_REGISTER ) {
-        print_error( &ast->loc, "function can not be register" );
-        return false;
-      }
-      // no break;
-    case K_BLOCK: {                     // Apple extension
-      c_ast_t const *const ret_ast = ast->as.func.ret_ast;
-      switch ( ret_ast->type ) {
-        case K_ARRAY:
-          print_error( &ret_ast->loc,
-            "%s returning array", c_kind_name( ast->kind )
-          );
-          print_hint( "pointer" );
-          return false;
-        case K_FUNCTION:
-          print_error( &ret_ast->loc,
-            "%s returning function", c_kind_name( ast->kind )
-          );
-          print_hint( "pointer to function" );
-          return false;
-        default:
-          return c_ast_check_func_args( ast ) && c_ast_check_impl( ret_ast );
-      } // switch
-    }
-
-    case K_NONE:
-      assert( ast->kind != K_NONE );
-
-    case K_POINTER:
-    case K_POINTER_TO_MEMBER: {
-      c_ast_t const *const to_ast = ast->as.ptr_ref.to_ast;
-      if ( to_ast->kind & K_REFERENCE ) {
-        print_error( &ast->loc, "%s to reference", c_kind_name( ast->kind ) );
-        return false;
-      }
-      if ( to_ast->type & T_REGISTER ) {
-        print_error( &ast->loc, "%s to register", c_kind_name( ast->kind ) );
-        return false;
-      }
-      return c_ast_check_impl( ast->as.ptr_ref.to_ast );
-    }
-
-    case K_REFERENCE: {
-      c_ast_t const *const to_ast = ast->as.ptr_ref.to_ast;
-      if ( to_ast->type & T_REGISTER ) {
-        print_error( &ast->loc, "reference to register" );
-        return false;
-      }
-      if ( to_ast->type & T_VOID ) {
-        print_error( &ast->loc, "reference to void" );
-        print_hint( "pointer to void" );
-        return false;
-      }
-      return c_ast_check_impl( to_ast );
-    }
-  } // switch
-
-  return true;
-}
-
-bool c_ast_check( c_ast_t const *ast, c_check_t check ) {
-  if ( check == CHECK_CAST && !c_ast_check_cast( ast ) )
-    return false;
-  return c_ast_check_impl( ast );
 }
 
 char const* c_ast_name( c_ast_t const *ast, v_direction_t dir ) {
