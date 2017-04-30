04f98b869c65148f3aae0f2b290b7e2470c8e96f
Paul J. Lucas <plucas@splunk.com>
2017-04-30 07:00:14 -0700

    Fixed missing ',' after class_name.

--- src/debug.c
+++ src/debug.c
@@ -112,7 +112,7 @@ void c_ast_debug( c_ast_t const *ast, unsigned indent, char const *key0,
       case K_POINTER_TO_MEMBER:
         PRINT_COMMA;
         INDENT_PRINT_KV( "class_name", ast->as.ptr_mbr.class_name );
-        FPUTC( '\n', dout );
+        FPUTS( ",\n", dout );
         // FALLTHROUGH
 
       case K_POINTER:
