feb380255b137332fbddc3f003f30ce02a09249d
Paul J. Lucas <plucas@splunk.com>
2019-12-12 19:16:07 -0800

    Doc tweaks.

--- src/util.h
+++ src/util.h
@@ -475,11 +475,12 @@ CDECL_UTIL_INLINE bool exactly_one_bit_set( uint64_t n ) {
 }
 
 /**
- * Checks the flag: if `false`, sets it to `true`.
+ * Checks \a flag: if `false`, sets it to `true`.
  *
  * @param flag A pointer to the Boolean flag to be tested and, if `false`,
  * sets it to `true`.
  * @return Returns `true` only if `*flag` is `false` initially.
+ * @sa true_or_set()
  */
 CDECL_UTIL_INLINE bool false_set( bool *flag ) {
   return !*flag && (*flag = true);
@@ -627,11 +628,12 @@ char* chrcpy_end( char *dst, char c );
 char* strcpy_end( char *dst, char const *src );
 
 /**
- * Checks the flag: if `false`, sets it to `true`.
+ * Checks \a flag: if `false`, sets it to `true`.
  *
  * @param flag A pointer to the Boolean flag to be tested and, if `false`, sets
  * it to `true`.
  * @return Returns `true` only if `*flag` is `true` initially.
+ * @sa false_set()
  */
 CDECL_UTIL_INLINE bool true_or_set( bool *flag ) {
   return *flag || !(*flag = true);
