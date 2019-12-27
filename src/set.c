ffc5c67d9d407611b58dae119ff3efdfa5fef1f2
Paul J. Lucas <plucas@splunk.com>
2019-12-26 18:48:07 -0800

    Now ignoring hyphens for set options.

--- src/set.c
+++ src/set.c
@@ -35,6 +35,7 @@
 /// @cond DOXYGEN_IGNORE
 
 // standard
+#include <stdint.h>                     /* for uint8_t */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -180,6 +181,30 @@ static void set_yydebug( bool enabled, c_loc_t const *loc ) {
 }
 #endif /* YYDEBUG */
 
+/**
+ * Compares strings ignoring hyphens.
+ *
+ * @param s1 The first string.
+ * @param s2 The second string.
+ * @param n The maximum number of characters to check.
+ * @return Returns an integer less than, equal to, or greater than 0 depending
+ * upon whether \a s1 is less than, equal to, or greater than \a s2.
+ */
+static int strn_nohyphen_cmp( char const *s1, char const *s2, size_t n ) {
+  while ( n-- > 0 ) {
+    if ( *s1 == '-' )
+      ++s1;
+    else if ( *s2 == '-' )
+      ++s2;
+    else if ( (int)(uint8_t)*s1 != (int)(uint8_t)*s2 )
+      break;
+    else
+      ++s1, ++s2;
+  } // while
+
+  return (int)(uint8_t)*s1 - (int)(uint8_t)*s2;
+}
+
 ////////// extern functions ///////////////////////////////////////////////////
 
 /**
@@ -232,7 +257,7 @@ void set_option( c_loc_t const *loc, char const *opt_name ) {
   for ( set_option_t const *opt = SET_OPTIONS; opt->name != NULL; ++opt ) {
     if ( !is_no && opt->no_only )
       continue;
-    if ( strncmp( opt->name, opt_name, opt_name_len ) == 0 ) {
+    if ( strn_nohyphen_cmp( opt->name, opt_name, opt_name_len ) == 0 ) {
       if ( found_opt != NULL ) {
         print_error( loc,
           "\"%s\": ambiguous set option; could be \"%s%s\" or \"%s%s\"",
