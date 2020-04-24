eeda2330de00318bd36b407195a101aeba26a7d2
Paul J. Lucas <plucas@splunk.com>
2020-04-24 10:26:36 -0700

    Tweaked c_alignas.

--- src/c_ast.h
+++ src/c_ast.h
@@ -95,8 +95,8 @@ enum c_alignas_arg {
  */
 struct c_alignas {
   union {
-    c_ast_t      *type_ast;             ///< Aligned the same as this type.
     unsigned      expr;                 ///< Aligned to this number of bytes.
+    c_ast_t      *type_ast;             ///< Aligned the same as this type.
   } as;                                 ///< Union discriminator.
   c_alignas_arg_t kind;                 ///< Kind of `alignas` argument.
 };
