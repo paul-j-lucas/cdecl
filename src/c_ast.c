b5e4fbe194364c28f54e21a537b395a5293e16b2
Paul J. Lucas <paul@lucasmail.org>
2020-07-17 21:38:14 -0700

    Reworked c_ast_sname_set_sname().

--- src/c_ast.c
+++ src/c_ast.c
@@ -279,16 +279,17 @@ void c_ast_sname_set_sname( c_ast_t *ast, c_sname_t *sname ) {
   assert( sname != NULL );
   c_sname_free( &ast->sname );
 
+  c_type_id_t sn_type = c_sname_type( sname );
+
+  c_ast_sname_append_sname( ast, sname );
+
   //
   // If the scoped name has no scope but the AST is one of a class, namespace,
   // struct, or union type, adopt that type for the scoped type.
   //
-  c_type_id_t sn_type = c_sname_type( sname );
   if ( sn_type == T_NONE && (ast->type_id & T_ANY_SCOPE) != T_NONE )
     sn_type = ast->type_id & (T_ANY_SCOPE | T_INLINE);
   c_ast_sname_set_type( ast, sn_type );
-
-  c_ast_sname_append_sname( ast, sname );
 }
 
 c_ast_t* c_ast_visit( c_ast_t *ast, c_visit_dir_t dir, c_ast_visitor_t visitor,
