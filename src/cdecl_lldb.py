350fcfe9138d25526a6511d1a80d835f353ff835
Paul J. Lucas <paul@lucasmail.org>
2020-11-01 07:17:45 -0800

    Removed "et al.".

--- src/cdecl_lldb.py
+++ src/cdecl_lldb.py
@@ -2,7 +2,7 @@
 #	cdecl -- C gibberish translator
 #	src/cdecl_lldb.py
 #
-#	Copyright (C) 2020  Paul J. Lucas, et al.
+#	Copyright (C) 2020  Paul J. Lucas
 #
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
