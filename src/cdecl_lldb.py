6a956ef5a2c8305f89ef0fd98a7de275e5f1a6d3
Paul J. Lucas <paul@lucasmail.org>
2020-11-02 10:11:40 -0800

    Tweaked quotes.

--- src/cdecl_lldb.py
+++ src/cdecl_lldb.py
@@ -39,10 +39,10 @@ def show_c_scope_t(c_scope, internal_dict):
     c_scope_data_ptr_t = target.FindFirstType('c_scope_data_t').GetPointerType()
     rv = ""
 
-    void_data_ptr = c_scope.GetChildMemberWithName("data")
+    void_data_ptr = c_scope.GetChildMemberWithName('data')
     if not null(void_data_ptr):
         c_scope_data_ptr = void_data_ptr.Cast(c_scope_data_ptr_t)
-        name_ptr = c_scope_data_ptr.GetChildMemberWithName("name")
+        name_ptr = c_scope_data_ptr.GetChildMemberWithName('name')
         if not null(name_ptr):
             rv = name_ptr.GetSummary().strip('"')
 
@@ -56,19 +56,19 @@ def show_c_sname_t(c_sname, internal_dict):
     colon2 = False
     rv = ""
 
-    slist_node_ptr = c_sname.GetChildMemberWithName("head")
+    slist_node_ptr = c_sname.GetChildMemberWithName('head')
     while not null(slist_node_ptr):
-        void_data_ptr = slist_node_ptr.GetChildMemberWithName("data")
+        void_data_ptr = slist_node_ptr.GetChildMemberWithName('data')
         if not null(void_data_ptr):
             c_scope_data_ptr = void_data_ptr.Cast(c_scope_data_ptr_t)
-            name_ptr = c_scope_data_ptr.GetChildMemberWithName("name")
+            name_ptr = c_scope_data_ptr.GetChildMemberWithName('name')
             if not null(name_ptr):
                 if colon2:
-                    rv += "::"
+                    rv += '::'
                 else:
                     colon2 = True
                 rv += name_ptr.GetSummary().strip('"')
-        slist_node_ptr = slist_node_ptr.GetChildMemberWithName("next")
+        slist_node_ptr = slist_node_ptr.GetChildMemberWithName('next')
 
     return '"' + rv + '"'
 
