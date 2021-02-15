3f40abf3ff7b202b0d94b61bcb5ea2dcadb94f51
Paul J. Lucas <paul@lucasmail.org>
2021-02-15 10:25:17 -0800

    Fixed size of until_buf.

--- src/c_lang.c
+++ src/c_lang.c
@@ -155,7 +155,7 @@ char const* c_lang_until( c_lang_id_t lang_ids ) {
   lang_ids &= mask;
   assert( lang_ids != LANG_NONE );
 
-  static char until_buf[ LANG_NAME_SIZE_MAX + 1/*\0*/ ];
+  static char until_buf[ 7/* until */ + LANG_NAME_SIZE_MAX + 1/*\0*/ ];
   sprintf( until_buf, " until %s", c_lang_oldest_name( lang_ids ) );
   return until_buf;
 }
