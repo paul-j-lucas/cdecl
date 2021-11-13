5b08bb0842c474d640f91ff4f03729e3acdd6cc0
Paul J. Lucas <paul@lucasmail.org>
2021-11-13 06:39:13 -0800

    Doxygen.

--- src/c_ast.h
+++ src/c_ast.h
@@ -346,7 +346,8 @@ struct c_ptr_ref_ast {
  *
  * @note Even though <code>%c_typedef_ast</code> has an AST pointer as its
  * first `struct` member, it is _not_ a parent "of" the underlying type, but
- * instead a synonym "for" it; hence, it's _not_ included in #K_ANY_PARENT.
+ * instead a synonym "for" it; hence, it's _not_ included in #K_ANY_PARENT, but
+ * it is, however, included in #K_ANY_REFERRER.
  *
  * @note C++ `using` declarations are stored as their equivalent `typedef`
  * declarations.
