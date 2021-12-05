3b6107af85c8c2c27b5d63363d0b3d532dcf949e
Paul J. Lucas <paul@lucasmail.org>
2021-12-05 07:50:33 -0800

    Removed "et al" from files that are wholly new.

--- src/check.c
+++ src/check.c
@@ -2,7 +2,7 @@
 **      cdecl -- C gibberish translator
 **      src/check.c
 **
-**      Copyright (C) 2017-2021  Paul J. Lucas, et al.
+**      Copyright (C) 2017-2021  Paul J. Lucas
 **
 **      This program is free software: you can redistribute it and/or modify
 **      it under the terms of the GNU General Public License as published by
