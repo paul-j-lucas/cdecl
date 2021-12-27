63e6781e0caec8fcbb9aa854301e441e1e75b12b
Paul J. Lucas <paul@lucasmail.org>
2021-12-27 08:23:16 -0800

    Doxygen.

--- src/c_ast_util.c
+++ src/c_ast_util.c
@@ -49,13 +49,16 @@ static c_ast_t* c_ast_append_array( c_ast_t*, c_ast_t*, c_ast_t* );
 ////////// local functions ////////////////////////////////////////////////////
 
 /**
- * Adds an array to the AST being built.
+ * Helper function for c_ast_add_array() that adds an array to the AST being
+ * built.
  *
  * @param ast The AST to append to; may be NULL.
  * @param array_ast The array AST to append.  Its `of_ast` must be of kind
  * #K_PLACEHOLDER.
  * @param of_ast The AST to become the `of_ast` of \a array_ast.
  * @return Returns the AST to be used as the grammar production's return value.
+ *
+ * @sa c_ast_append_array()
  */
 PJL_WARN_UNUSED_RESULT
 static c_ast_t* c_ast_add_array_impl( c_ast_t *ast, c_ast_t *array_ast,
@@ -140,6 +143,7 @@ static c_ast_t* c_ast_add_array_impl( c_ast_t *ast, c_ast_t *array_ast,
 }
 
 /**
+ * Helper function for c_ast_add_array_impl().
  * If \a ast is:
  *  + Not an array, makes \a array_ast an array of \a ast.
  *  + An array, appends \a array_ast to the end of the array AST chain.
@@ -159,6 +163,8 @@ static c_ast_t* c_ast_add_array_impl( c_ast_t *ast, c_ast_t *array_ast,
  * @param of_ast The AST to become the `of_ast` of \a array_ast.
  * @return If \a ast is an array, returns \a ast; otherwise returns \a
  * array_ast.
+ *
+ * @sa c_ast_add_array_impl()
  */
 PJL_WARN_UNUSED_RESULT
 static c_ast_t* c_ast_append_array( c_ast_t *ast, c_ast_t *array_ast,
