e756df2813482ac5653291c22462c2d2a21b16da
Paul J. Lucas <paul@lucasmail.org>
2022-02-19 07:37:45 -0800

    s/yy_token_id/y_token_id/

--- src/cdecl_keyword.h
+++ src/cdecl_keyword.h
@@ -51,13 +51,13 @@ struct cdecl_keyword {
   bool                always_find;      ///< Find even when explaining C/C++?
 
   /**
-   * The Bison token ID (`Y_xxx`), but only if lang_syn is NULL; otherwise 0.
+   * The Bison token (`Y_xxx`), but only if lang_syn is NULL; otherwise 0.
    */
-  yytokentype         yy_token_id;
+  yytokentype         y_token_id;
 
   /**
-   * Array of language(s)/synonym-keyword pair(s), but only if yy_token_id is
-   * 0; otherwise NULL.
+   * Array of language(s)/synonym-keyword pair(s), but only if y_token_id is 0;
+   * otherwise NULL.
    *
    * The array is terminated by an element that has #LANG_ANY for lang_ids;
    * hence subset(s) of language(s) cases come first and, failing to match
