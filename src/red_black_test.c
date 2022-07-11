3bbd941bfc88dfa79de196cee16262aaa0ebef8d
Paul J. Lucas <paul@lucasmail.org>
2022-07-11 13:32:50 -0700

    Added complete tree validation.

--- src/red_black_test.c
+++ src/red_black_test.c
@@ -47,6 +47,28 @@ static int test_rb_data_cmp( void const *i_data, void const *j_data ) {
   return strcmp( i_str, j_str );
 }
 
+static void test_rb_validate_node( rb_tree_t const *tree,
+                                   rb_node_t const *node ) {
+  if ( node->color == RB_RED ) {
+    TEST( node->child[0]->color == RB_BLACK );
+    TEST( node->child[1]->color == RB_BLACK );
+  }
+
+  if ( node->child[0] != &tree->nil ) {
+    TEST( (*tree->cmp_fn)( node->child[0]->data, node->data ) <= 0 );
+    test_rb_validate_node( tree, node->child[0] );
+  }
+
+  if ( node->child[1] != &tree->nil ) {
+    TEST( (*tree->cmp_fn)( node->data, node->child[1]->data ) <= 0 );
+    test_rb_validate_node( tree, node->child[1] );
+  }
+}
+
+static void test_rb_validate( rb_tree_t const *tree ) {
+  test_rb_validate_node( tree, tree->fake_root.child[0] );
+}
+
 static bool test_rb_visitor( void *node_data, void *v_data ) {
   char const *const str = node_data;
   unsigned *const letter_offset_ptr = v_data;
@@ -70,9 +92,11 @@ static void test_insert1_find_delete( void ) {
   rb_tree_init( &tree, &test_rb_data_cmp );
 
   if ( TEST( rb_tree_insert( &tree, (void*)"A" ).inserted ) ) {
+    test_rb_validate( &tree );
     rb_node_t *const node = rb_tree_find( &tree, (void*)"A" );
     if ( TEST( node != NULL ) ) {
       void *const data = rb_tree_delete( &tree, node );
+      test_rb_validate( &tree );
       if ( TEST( data != NULL ) )
         TEST( strcmp( data, "A" ) == 0 );
     }
@@ -88,9 +112,11 @@ static void test_insert2_find_delete( void ) {
 
   if ( TEST( rb_tree_insert( &tree, (void*)"A" ).inserted ) &&
        TEST( rb_tree_insert( &tree, (void*)"B" ).inserted ) ) {
+    test_rb_validate( &tree );
     rb_node_t *const node = rb_tree_find( &tree, (void*)"B" );
     if ( TEST( node != NULL ) ) {
       void *const data = rb_tree_delete( &tree, node );
+      test_rb_validate( &tree );
       if ( TEST( data != NULL ) )
         TEST( strcmp( data, "B" ) == 0 );
     }
@@ -117,12 +143,17 @@ int main( int argc, char const *argv[] ) {
 
   // test insertion
   TEST( rb_tree_insert( &tree, (void*)"A" ).inserted );
+  test_rb_validate( &tree );
   TEST( rb_tree_insert( &tree, (void*)"B" ).inserted );
+  test_rb_validate( &tree );
   TEST( rb_tree_insert( &tree, (void*)"C" ).inserted );
+  test_rb_validate( &tree );
   TEST( rb_tree_insert( &tree, (void*)"D" ).inserted );
+  test_rb_validate( &tree );
 
   // test insertion with existing data
   rb_insert_rv = rb_tree_insert( &tree, (void*)"A" );
+  test_rb_validate( &tree );
   if ( TEST( !rb_insert_rv.inserted ) )
     TEST( strcmp( rb_insert_rv.node->data, "A" ) == 0 );
 
@@ -137,6 +168,7 @@ int main( int argc, char const *argv[] ) {
 
     // test delete
     void *const data = rb_tree_delete( &tree, node );
+    test_rb_validate( &tree );
     if ( TEST( data != NULL ) ) {
       TEST( strcmp( data, "A" ) == 0 );
 
