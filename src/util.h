273a91856379af1a85da2cbed40d8fe1c99602c3
Paul J. Lucas <paul@lucasmail.org>
2023-04-18 10:46:49 -0700

    Doxygen.

--- src/util.h
+++ src/util.h
@@ -1229,8 +1229,8 @@ bool true_clear( bool *flag ) {
  * Possibly prints the list separator \a sep based on \a sep_flag.
  *
  * @param sep The separator to print.
- * @param sep_flag A pointer to a flag to know whether to print \a sep.  The
- * flag should be `false` initially.
+ * @param sep_flag If `true`, prints \a sep; if `false`, prints nothing, but
+ * sets it flag to `true`.  The flag should be `false` initially.
  * @param sout The `FILE` to print to.
  */
 C_UTIL_H_INLINE
