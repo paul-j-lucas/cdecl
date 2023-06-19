e047a6219743724bca276a91bdd44b56d9b31c29
Paul J. Lucas <paul@lucasmail.org>
2023-06-19 07:57:46 -0700

    Now checking for unused *.out files.

--- test/not_in_Makefile.sh
+++ test/not_in_Makefile.sh
@@ -19,10 +19,26 @@
 #       along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
 
-# Prints *.test files that exist but are not in Makefile.am.
+local_basename() {
+  ##
+  # Autoconf, 11.15:
+  #
+  # basename
+  #   Not all hosts have a working basename. You can use expr instead.
+  ##
+  expr "//$1" : '.*/\(.*\)'
+}
 
+# Prints *.test files that exist but are not in Makefile.am.
 ls tests/*.test | while read TEST
 do fgrep $TEST Makefile.am >/dev/null 2>&1 || echo $TEST
+done
+
+# Prints *.out files that exist but have no corresponding test in Makefile.am.
+ls expected/*.out | while read EXPECTED
+do
+  TEST=`local_basename "$EXPECTED" | sed s/out/test/`
+  fgrep $TEST Makefile.am >/dev/null 2>&1 || echo $EXPECTED
 done
 
 # vim:set et sw=2 ts=2:
