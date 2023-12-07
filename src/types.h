d7b53160a8f9cf95a18b4272a1594eb2ead6d8d3
Paul J. Lucas <paul@lucasmail.org>
2023-12-06 18:33:22 -0800

    Cosmetic.

--- src/types.h
+++ src/types.h
@@ -267,6 +267,17 @@ typedef c_loc_t YYLTYPE;                ///< Source location type for Bison.
 #define YYLTYPE_IS_TRIVIAL        1
 /// @endcond
 
+/**
+ * The signature for a function passed to **qsort**(3).
+ *
+ * @param i_data A pointer to data.
+ * @param j_data A pointer to data.
+ * @return Returns an integer less than, equal to, or greater than 0, according
+ * to whether the data pointed to by \a i_data is less than, equal to, or
+ * greater than the data pointed to by \a j_data.
+ */
+typedef int (*qsort_cmp_fn_t)( void const *i_data, void const *j_data );
+
 ////////// structs ////////////////////////////////////////////////////////////
 
 /**
@@ -317,17 +328,6 @@ struct c_ast_pair {
    */
   c_ast_t *target_ast;
 };
-
-/**
- * The signature for a function passed to **qsort**(3).
- *
- * @param i_data A pointer to data.
- * @param j_data A pointer to data.
- * @return Returns an integer less than, equal to, or greater than 0, according
- * to whether the data pointed to by \a i_data is less than, equal to, or
- * greater than the data pointed to by \a j_data.
- */
-typedef int (*qsort_cmp_fn_t)( void const *i_data, void const *j_data );
 
 /**
  * "User data" passed as additional data to certain functions.
