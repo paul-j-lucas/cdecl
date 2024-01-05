77ad1e1a56330c8a58df23546d0e7f86823feb27
Paul J. Lucas <paul@lucasmail.org>
2024-01-05 07:11:48 -0800

    More red-black tree tests.

--- src/red_black_test.c
+++ src/red_black_test.c
@@ -31,36 +31,89 @@
 #include <string.h>
 #include <sysexits.h>
 
+#define RB_NIL(TREE)              (&(TREE)->nil)
+
 ///////////////////////////////////////////////////////////////////////////////
+
+enum rb_test_cmd {
+  RB_TEST_INSERT,
+  RB_TEST_DELETE
+};
+typedef enum rb_test_cmd rb_test_cmd_t;
+
+struct rb_test_instruction {
+  rb_test_cmd_t   cmd;
+  char const     *key;
+};
+typedef struct rb_test_instruction rb_test_instruction_t;
 
 // extern variables
 char const       *me;                   ///< Program name.
+
+// This sequence of instructions used to cause the tree's invariants to break,
+// so it's now a test.
+static rb_test_instruction_t const RB_TEST_SCRIPT[] = {
+  { RB_TEST_INSERT, "cx_throw()" },
+  { RB_TEST_INSERT, "CX_IMPL_DEF_ARGS(CX_IMPL_THROW_,)" },
+  { RB_TEST_INSERT, "CX_IMPL_NAME2(CX_IMPL_THROW_, CX_IMPL_NARG())" },
+  { RB_TEST_INSERT, "CX_IMPL_NARG()" },
+  { RB_TEST_INSERT, "CX_IMPL_NARG_HELPER1(CX_IMPL_HAS_COMMA( ), CX_IMPL_HAS_COMMA( CX_IMPL_COMMA () ), CX_IMPL_NARG_( , CX_IMPL_REV_SEQ_N ))" },
+  { RB_TEST_INSERT, "CX_IMPL_HAS_COMMA()" },
+  { RB_TEST_INSERT, "CX_IMPL_HAS_COMMA_N" },
+  { RB_TEST_DELETE, "CX_IMPL_HAS_COMMA_N" },
+  { RB_TEST_INSERT, "CX_IMPL_HAS_COMMA_N" },
+  { RB_TEST_INSERT, "CX_IMPL_ARG_N(, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0)" },
+  { RB_TEST_DELETE, "CX_IMPL_ARG_N(, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0)" },
+  { RB_TEST_DELETE, "CX_IMPL_HAS_COMMA_N" },
+  { RB_TEST_DELETE, "CX_IMPL_HAS_COMMA()" },
+  { RB_TEST_INSERT, "CX_IMPL_COMMA" },
+  { RB_TEST_INSERT, "CX_IMPL_HAS_COMMA_N" },
+  { RB_TEST_DELETE, "CX_IMPL_HAS_COMMA_N" },
+  { RB_TEST_INSERT, "CX_IMPL_HAS_COMMA_N" },
+  { RB_TEST_INSERT, "CX_IMPL_ARG_N(CX_IMPL_COMMA (), 1, 1, 1, 1, 1, 1, 1, 1, 0, 0)" },
+  { RB_TEST_DELETE, "CX_IMPL_ARG_N(CX_IMPL_COMMA (), 1, 1, 1, 1, 1, 1, 1, 1, 0, 0)" },
+  { RB_TEST_DELETE, "CX_IMPL_HAS_COMMA_N" },
+  { RB_TEST_DELETE, "CX_IMPL_COMMA" },
+  { RB_TEST_INSERT, "CX_IMPL_REV_SEQ_N" },
+  { RB_TEST_DELETE, "CX_IMPL_REV_SEQ_N" },
+  { RB_TEST_INSERT, "CX_IMPL_REV_SEQ_N" },
+  { RB_TEST_INSERT, "CX_IMPL_ARG_N(, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)" },
+  { RB_TEST_DELETE, "CX_IMPL_ARG_N(, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)" },
+  { RB_TEST_DELETE, "CX_IMPL_REV_SEQ_N" },
+  { RB_TEST_INSERT, "CX_IMPL_NARG_HELPER2(0, 0, 1)" },
+  { RB_TEST_INSERT, "CX_IMPL_NARG_HELPER3_00(1)" },
+  { RB_TEST_DELETE, "CX_IMPL_NARG_HELPER3_00(1)" },
+  { RB_TEST_DELETE,  "CX_IMPL_NARG_HELPER2(0, 0, 1)" },
+  { RB_TEST_DELETE, "CX_IMPL_NARG_HELPER1(CX_IMPL_HAS_COMMA( ), CX_IMPL_HAS_COMMA( CX_IMPL_COMMA () ), CX_IMPL_NARG_( , CX_IMPL_REV_SEQ_N ))" },
+};
 
 // local variables
 static unsigned   test_failures;
 
 ////////// local functions ////////////////////////////////////////////////////
 
-static void test_rb_validate_node( rb_tree_t const *tree,
-                                   rb_node_t const *node ) {
+static void test_check_rb_node( rb_tree_t const *tree, rb_node_t const *node ) {
   if ( node->color == RB_RED ) {
     TEST( node->child[0]->color == RB_BLACK );
     TEST( node->child[1]->color == RB_BLACK );
   }
 
-  if ( node->child[0] != &tree->nil ) {
+  if ( node->child[0] != RB_NIL(tree) ) {
     TEST( (*tree->cmp_fn)( node->child[0]->data, node->data ) <= 0 );
-    test_rb_validate_node( tree, node->child[0] );
+    test_check_rb_node( tree, node->child[0] );
   }
 
-  if ( node->child[1] != &tree->nil ) {
+  if ( node->child[1] != RB_NIL(tree) ) {
     TEST( (*tree->cmp_fn)( node->data, node->child[1]->data ) <= 0 );
-    test_rb_validate_node( tree, node->child[1] );
+    test_check_rb_node( tree, node->child[1] );
   }
 }
 
-static void test_rb_validate( rb_tree_t const *tree ) {
-  test_rb_validate_node( tree, tree->root );
+static void test_check_rb_tree( rb_tree_t const *tree ) {
+  TEST( tree->root != NULL );
+  TEST( tree->root->color == RB_BLACK );
+  TEST( RB_NIL(tree)->color == RB_BLACK );
+  test_check_rb_node( tree, tree->root );
 }
 
 static bool test_rb_visitor( void *node_data, void *v_data ) {
@@ -87,11 +140,11 @@ static void test_insert1_find_delete( void ) {
   rb_tree_init( &tree, POINTER_CAST( rb_cmp_fn_t, &strcmp ) );
 
   if ( TEST( rb_tree_insert( &tree, (void*)"A" ).inserted ) ) {
-    test_rb_validate( &tree );
+    test_check_rb_tree( &tree );
     rb_node_t *const node = rb_tree_find( &tree, (void*)"A" );
     if ( TEST( node != NULL ) ) {
       void *const data = rb_tree_delete( &tree, node );
-      test_rb_validate( &tree );
+      test_check_rb_tree( &tree );
       if ( TEST( data != NULL ) )
         TEST( strcmp( data, "A" ) == 0 );
     }
@@ -107,15 +160,40 @@ static void test_insert2_find_delete( void ) {
 
   if ( TEST( rb_tree_insert( &tree, (void*)"A" ).inserted ) &&
        TEST( rb_tree_insert( &tree, (void*)"B" ).inserted ) ) {
-    test_rb_validate( &tree );
+    test_check_rb_tree( &tree );
     rb_node_t *const node = rb_tree_find( &tree, (void*)"B" );
     if ( TEST( node != NULL ) ) {
       void *const data = rb_tree_delete( &tree, node );
-      test_rb_validate( &tree );
+      test_check_rb_tree( &tree );
       if ( TEST( data != NULL ) )
         TEST( strcmp( data, "B" ) == 0 );
     }
   }
+
+  rb_tree_cleanup( &tree, /*free_fn=*/NULL );
+  TEST( rb_tree_empty( &tree ) );
+}
+
+static void test_script( void ) {
+  rb_tree_t tree;
+  rb_tree_init( &tree, POINTER_CAST( rb_cmp_fn_t, &strcmp ) );
+
+  FOREACH_ARRAY_ELEMENT( rb_test_instruction_t, i, RB_TEST_SCRIPT ) {
+    switch ( i->cmd ) {
+      case RB_TEST_INSERT:
+        TEST( rb_tree_insert( &tree, (void*)i->key ).inserted );
+        break;
+      case RB_TEST_DELETE:
+        NO_OP;
+        rb_node_t *const found = rb_tree_find( &tree, (void*)i->key );
+        if ( TEST( found != NULL ) ) {
+          char const *const data = rb_tree_delete( &tree, found );
+          if ( TEST( data != NULL ) )
+            TEST( strcmp( data, i->key ) == 0 );
+        }
+        break;
+    } // switch
+  } // for
 
   rb_tree_cleanup( &tree, /*free_fn=*/NULL );
   TEST( rb_tree_empty( &tree ) );
@@ -138,17 +216,17 @@ int main( int argc, char const *argv[const] ) {
 
   // test insertion
   TEST( rb_tree_insert( &tree, (void*)"A" ).inserted );
-  test_rb_validate( &tree );
+  test_check_rb_tree( &tree );
   TEST( rb_tree_insert( &tree, (void*)"B" ).inserted );
-  test_rb_validate( &tree );
+  test_check_rb_tree( &tree );
   TEST( rb_tree_insert( &tree, (void*)"C" ).inserted );
-  test_rb_validate( &tree );
+  test_check_rb_tree( &tree );
   TEST( rb_tree_insert( &tree, (void*)"D" ).inserted );
-  test_rb_validate( &tree );
+  test_check_rb_tree( &tree );
 
   // test insertion with existing data
   rb_insert_rv = rb_tree_insert( &tree, (void*)"A" );
-  test_rb_validate( &tree );
+  test_check_rb_tree( &tree );
   if ( TEST( !rb_insert_rv.inserted ) )
     TEST( strcmp( rb_insert_rv.node->data, "A" ) == 0 );
 
@@ -163,7 +241,7 @@ int main( int argc, char const *argv[const] ) {
 
     // test delete
     void *const data = rb_tree_delete( &tree, node );
-    test_rb_validate( &tree );
+    test_check_rb_tree( &tree );
     if ( TEST( data != NULL ) ) {
       TEST( strcmp( data, "A" ) == 0 );
 
@@ -175,6 +253,8 @@ int main( int argc, char const *argv[const] ) {
 
   rb_tree_cleanup( &tree, /*free_fn=*/NULL );
   TEST( rb_tree_empty( &tree ) );
+
+  test_script();
 
   printf( "%u failures\n", test_failures );
   exit( test_failures > 0 ? EX_SOFTWARE : EX_OK );
