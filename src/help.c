b09491e9bd03a94ea3b932a9aaf52bc40ca074a9
Paul J. Lucas <paul@lucasmail.org>
2024-06-09 07:09:24 -0700

    Added help for "scope-e."

--- src/help.c
+++ src/help.c
@@ -49,6 +49,7 @@
 
 // local functions
 static void print_help_name_number( void );
+static void print_help_scope( void );
 static void print_help_where( void );
 
 /**
@@ -375,12 +376,8 @@ static void print_help_command( cdecl_command_t const *command ) {
   if ( command_is_any( command, L_PRE_P_define, L_expand, NULL ) )
     print_h( "pp-token: a preprocessor token\n" );
 
-  if ( command == NULL && OPT_LANG_IS( SCOPED_NAMES ) ) {
-    print_h( "scope-c: class | struct | union |" );
-    if ( OPT_LANG_IS( inline_namespace ) )
-      print_h( " [inline]" );
-    print_h( " namespace\n" );
-  }
+  if ( command == NULL && OPT_LANG_IS( SCOPED_NAMES ) )
+    print_help_scope();
 
   if ( command == NULL )
     print_help_where();
@@ -559,11 +556,7 @@ static void print_help_english( void ) {
 
     print_h( "modifier: short | long | signed | unsigned | <cv-qual>\n" );
     print_help_name_number();
-
-    print_h( "scope-e: scope | class | struct | union |" );
-    if ( OPT_LANG_IS( inline_namespace ) )
-      print_h( " [inline]" );
-    print_h( " namespace\n" );
+    print_help_scope();
 
     print_h( "store:" );
     if ( OPT_LANG_IS( auto_STORAGE ) )
@@ -600,6 +593,19 @@ static void print_help_name_number( void ) {
   else
     print_h( "name: a C\\+\\+ identifier: <name>[::<name>]* | <name> [of <scope-e> <name>]*\n" );
   print_h( "number: a binary, octal, decimal, or hexadecimal integer\n" );
+}
+
+/**
+ * Prints help for **cdecl** scoped names.
+ */
+static void print_help_scope( void ) {
+  assert( OPT_LANG_IS( SCOPED_NAMES ) );
+
+  print_h( "scope-c: class | struct | union |" );
+  if ( OPT_LANG_IS( inline_namespace ) )
+    print_h( " [inline]" );
+  print_h( " namespace\n" );
+  print_h( "scope-e: <scope-c> | scope\n" );
 }
 
 /**
