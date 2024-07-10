b015f0c441ce1894293fc5bfb4f111c5d63b6214
Paul J. Lucas <paul@lucasmail.org>
2024-07-10 10:13:45 -0700

    Added missing c_alignas_dup().

--- src/c_ast.c
+++ src/c_ast.c
@@ -145,6 +145,34 @@ static void c_alignas_cleanup( c_alignas_t *align ) {
 }
 
 /**
+ * Duplicates \a align.
+ *
+ * @param align The \ref c_alignas to duplicate; may be NULL.
+ * @return Returns the duplicated alignment.
+ */
+static c_alignas_t c_alignas_dup( c_alignas_t const *align ) {
+  c_alignas_t dup_align = { 0 };
+  if ( align != NULL ) {
+    dup_align.kind = align->kind;
+    dup_align.loc = align->loc;
+    switch ( align->kind ) {
+      case C_ALIGNAS_NONE:
+        break;                          // LCOV_EXCL_LINE
+      case C_ALIGNAS_BYTES:
+        dup_align.bytes = align->bytes;
+        break;
+      case C_ALIGNAS_SNAME:
+        dup_align.sname = c_sname_dup( &align->sname );
+        break;
+      case C_ALIGNAS_TYPE:
+        dup_align.type_ast = align->type_ast;
+        break;
+    } // switch
+  }
+  return dup_align;
+}
+
+/**
  * Checks whether two alignments are equal.
  *
  * @param i_align The first alignment.
@@ -253,7 +281,7 @@ c_ast_t* c_ast_dup( c_ast_t const *ast, c_ast_list_t *node_list ) {
   c_ast_t *const dup_ast =
     c_ast_new( ast->kind, ast->depth, &ast->loc, node_list );
 
-  dup_ast->align = ast->align;
+  dup_ast->align = c_alignas_dup( &ast->align );
   dup_ast->dup_from_ast = ast;
   dup_ast->is_param_pack = ast->is_param_pack;
   dup_ast->sname = c_sname_dup( &ast->sname );
