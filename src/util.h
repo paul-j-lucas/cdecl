43e3e1795c83e4491d8e605a52b1bd0aa8146bc6
Paul J. Lucas <paul@lucasmail.org>
2024-07-13 06:03:53 -0700

    Cosmetic.

--- src/util.h
+++ src/util.h
@@ -103,11 +103,11 @@ _GL_INLINE_HEADER_BEGIN
 
 /// @cond DOXYGEN_IGNORE
 #define ARG_WITHIN_PARENS(...)    ,
-#define NAME5(A,B,C,D,E)          NAME5_HELPER( A, B, C, D, E )
-#define NAME5_HELPER(A,B,C,D,E)   A ## B ## C ## D ## E
 #define ARGS_IS_EMPTY_HELPER(_1,_2,_3,_4) \
   ARGS_HAS_COMMA( ARGS_5( ARGS_IS_EMPTY_RESULT_, _1, _2, _3, _4 ) )
 #define ARGS_IS_EMPTY_RESULT_0001 ,
+#define NAME5(A,B,C,D,E)          NAME5_HELPER( A, B, C, D, E )
+#define NAME5_HELPER(A,B,C,D,E)   A ## B ## C ## D ## E
 /// @endcond
 
 /**
