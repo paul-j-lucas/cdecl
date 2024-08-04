1d8ad057c721640fb74edd969982d682afb76c41
Paul J. Lucas <paul@lucasmail.org>
2024-08-03 17:28:19 -0700

    Doxygen.

--- src/options.h
+++ src/options.h
@@ -250,7 +250,7 @@ bool parse_explicit_ecsu( char const *ecsu_format );
  *    `i`                    | All signed integer types.
  * `u`                       | All unsigned integer types.
  * [`u`]{`i`\|`s`\|`l`[`l`]} | Possibly `unsigned` `int`, `short`, `long`, or `long long`.
-
+ *
  * Multiple formats may be given, one immediately after the other, e.g., `usl`
  * means `unsigned short` and `long`.  Parsing is greedy so commas may be used
  * to separate formats.  For example, `ulll` is parsed as `unsigned long long`
