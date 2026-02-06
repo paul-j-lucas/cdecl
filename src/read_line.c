661943f54edd309b588d6bbc531e102b1facebe9
Paul J. Lucas <paul@lucasmail.org>
2026-02-06 07:17:47 -0800

    Removed unnecessary #include <stddef.h>.

--- src/read_line.c
+++ src/read_line.c
@@ -37,7 +37,6 @@
 // standard
 #include <assert.h>
 #include <stdbool.h>
-#include <stddef.h>                     /* for NULL, size_t */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
